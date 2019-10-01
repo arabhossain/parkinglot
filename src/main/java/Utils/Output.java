8cd7eaf0a39f72e5e71fdf49681dc02a3c5175ec
Arab Hossain <green.arab1995@gmail.com>
2019-10-02 00:50:41 +0600

    Added Output Util

---
+++ src/main/java/Utils/Output.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utils;
+
+/**
+ *
+ * @author arab
+ */
+public class Output {
+ 
+    static public void Output (String message){
+        System.out.println(message);
+    }
+    
+    static public void Output(int message){
+        System.out.println(message);
+    }
+}
--- src/test/java/Utils/OutputTest.java
+++ src/test/java/Utils/OutputTest.java
@@ -5,10 +5,41 @@
  */
 package Utils;
 
+import static Utils.Output.Output;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author arab
  */
 public class OutputTest {
+    private final ByteArrayOutputStream printedContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+	System.setOut(new PrintStream(printedContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+	System.setOut(null);
+    }
+    
+    @Test
+    public void StringOutput(){
+        Output("TestString");
+        assertEquals("TestString", printedContent.toString().trim());
+    }
+    
+    @Test
+    public void numericOutput(){
+        Output(3);
+        assertEquals(3, Integer.parseInt(printedContent.toString().trim()));
+    }
     
 }
